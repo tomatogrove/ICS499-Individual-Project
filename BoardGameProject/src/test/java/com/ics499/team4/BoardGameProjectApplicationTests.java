0c6fc69da1e7d3f560283a84308b755d8f1a8619
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2023-01-29 11:29:43 -0600

    created starter project

---
+++ BoardGameProject/src/test/java/com/ics499/team4/BoardGameProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ics499.team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
