5724835b95884689b6aade40bb627cdd694b4ca5
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2023-02-03 18:49:43 -0600

    Create Game.java

---
+++ BoardGameProject/src/main/java/com/backend/abstrct/game/Game.java
@@ -0,0 +1,26 @@
+package com.backend.abstrct.game;
+
+import com.backend.classes.Player;
+import com.backend.classes.Rule;
+
+public abstract class Game {
+
+	private String type;
+	private Player[] players;
+	private Rule[] rules;
+	
+	public Game() {}
+	
+	public String getType() { return type; }
+
+	public void setType(String type) { this.type = type; }
+
+	public Player[] getPlayers() { return players; }
+
+	public void setPlayers(Player[] players) { this.players = players; }
+
+	public Rule[] getRules() { return rules; }
+
+	public void setRules(Rule[] rules) { this.rules = rules; }
+}
+
