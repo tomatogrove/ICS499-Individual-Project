f2542df3c19d8876aac55a42f473b637418750d9
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2023-02-08 20:02:47 -0600

    final changes to get successful launch!

--- BoardGameProject/src/main/java/com/team4/BoardGameProjectApplication.java
+++ BoardGameProject/src/main/java/com/team4/BoardGameProjectApplication.java
@@ -3,9 +3,13 @@ package com.team4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan( basePackages = {"com.team4.model"} )
+@EntityScan( basePackages = {"com.team4.model.*"} )
+@EnableJpaRepositories("com.team4.model.*")
+@ComponentScan( basePackages = { "com.team4.model.*" } )
 public class BoardGameProjectApplication {
 
 	public static void main(String[] args) {
