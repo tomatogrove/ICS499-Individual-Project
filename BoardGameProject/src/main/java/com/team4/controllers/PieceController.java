e7688e3ef28d8b59dfd45955acb1ca4104cb4331
Nat-Des <123428679+Nat-Des@users.noreply.github.com>
2023-02-09 12:05:47 -0600

    Controller for Piece class

---
+++ BoardGameProject/src/main/java/com/team4/controllers/PieceController.java
@@ -0,0 +1,18 @@
+package com.team4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team4.model.classes.Piece;
+
+@RestController
+public class PieceController {
+  
+    @GetMapping("/pieces/{id}")
+    public Piece getPieceById(@PathVariable Long id) {
+        // This is where you would query your database to retrieve the Piece with the given id
+        // For now, we're just returning a dummy piece
+        return new Piece(id, "Knight", "White");
+    }
+}
