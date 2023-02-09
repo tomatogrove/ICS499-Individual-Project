01cd14b941eb7f8e8d59c158e30f19a76b9f8486
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2023-02-08 19:06:35 -0600

    removed unused imports

--- BoardGameProject/src/main/java/com/team4/BoardGameProjectApplication.java
+++ BoardGameProject/src/main/java/com/team4/BoardGameProjectApplication.java
@@ -3,9 +3,6 @@ package com.team4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EntityScan( basePackages = {"com.team4.model"} )
