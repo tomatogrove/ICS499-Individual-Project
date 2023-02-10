9782c4dcec339332c6b881682b497ad3fcba0a9b
lormai9 <112415899+lormai9@users.noreply.github.com>
2023-02-09 18:24:45 -0600

    Update deck.java

--- BoardGameProject/src/main/java/com/team4/model/classes/deck.java
+++ BoardGameProject/src/main/java/com/team4/model/classes/deck.java
@@ -2,27 +2,15 @@ package com.backend.classes;
 
 
 public class deck {
-	private Card[] cards;
-	private int cardsInDeck;
+	private Card cards;
+	private Card draw;
+	public void shuffle();
 	
-	public deck()
-	{ 
-		cards = new Card[108]
+}
+public ArrayList<Card> getCard(){
+	return this.card;
+}
+public void deal(int numCards) {
+	this.numCards.deal(card);
 	}
-	public void reset()
-			card.color[]	 colors = card.color.values();
-			cardInDeck = 0;
-			
-			for(int i = 0; i < color.length-1; i++)
-			{
-				card.color colors = color[i];
-				
-				card[cardInDeck++] = new card(colors, card.Vakue.getValue(0));
-				
-				for(int j = 1; j < 10; j++)
-				{
-					card[cardInDeck++] = new card(colors, card.Value.getCalue(j));
-					card[cardInDeck++] = new card(colors, card.Value.getCalue(j));
-				}
-			}
 }
