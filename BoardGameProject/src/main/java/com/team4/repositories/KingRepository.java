070d719042e81ac651c851b890881da29e286b93
guledmo8 <guledmohamed8@gmail.com>
2023-02-12 17:16:54 -0600

    added repositories for Pawn and King

---
+++ BoardGameProject/src/main/java/com/team4/repositories/KingRepository.java
@@ -0,0 +1,10 @@
+package com.team4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team4.model.classes.King;
+
+public interface KingRepository extends JpaRepository<King, Long> {
+
+}
+
---
+++ BoardGameProject/src/main/java/com/team4/repositories/PawnRepository.java
@@ -0,0 +1,10 @@
+package com.team4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team4.model.classes.Pawn;
+
+public interface PawnRepository extends JpaRepository<Pawn, Long> {
+
+}
+
