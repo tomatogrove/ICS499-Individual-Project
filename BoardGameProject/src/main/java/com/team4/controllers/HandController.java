5f84af506962c512e7a2c55d09df8851970e691c
Nat-Des <123428679+Nat-Des@users.noreply.github.com>
2023-02-13 19:58:33 -0600

    Update HandController.java

--- BoardGameProject/src/main/java/com/team4/controllers/HandController.java
+++ BoardGameProject/src/main/java/com/team4/controllers/HandController.java
@@ -3,29 +3,46 @@ package com.team4.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team4.model.classes.Hand;
-import com.team4.service.HandService;
+import com.team4.repositories.HandRepository;
 
 @RestController
-@RequestMapping("/api/hand")
+@RequestMapping("/hand")
 public class HandController {
-	
 	@Autowired
-	private HandService handService;
-	
-	@GetMapping
-	public List<Hand> getAllHands() {
-		return handService.getAllHands();
+	private HandRepository handRepo;
+
+	@GetMapping("/all")
+	public List<Hand> list() {
+		return handRepo.findAll();
 	}
-	
-	@PostMapping
-	public Hand createHand(@RequestBody Hand hand) {
-		return handService.createHand(hand);
+
+	@GetMapping("/{id}")
+	public Hand get(@PathVariable Long id) {
+		return handRepo.getReferenceById(id);
 	}
+
+	@PostMapping("/add")
+	public Hand create(@RequestBody final Hand hand) {
+		return handRepo.saveAndFlush(hand);
+	}
+
+	@PutMapping("/update")
+	public Hand update(@RequestBody final Hand hand) {
+		return handRepo.saveAndFlush(hand);
+	}
+
+@DeleteMapping("/delete/{id}")
+public void delete(@PathVariable Long id) {
+	handRepo.deleteById(id);
+}
 }
