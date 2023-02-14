8782f8f93c8f855c39b556344451702c7f75361d
Nat-Des <123428679+Nat-Des@users.noreply.github.com>
2023-02-13 19:55:44 -0600

    Update PieceController.java

--- BoardGameProject/src/main/java/com/team4/controllers/PieceController.java
+++ BoardGameProject/src/main/java/com/team4/controllers/PieceController.java
@@ -1,18 +1,47 @@
 package com.team4.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team4.model.classes.Piece;
+import com.team4.repositories.PieceRepository;
 
 @RestController
+@RequestMapping("/piece")
 public class PieceController {
-  
-    @GetMapping("/pieces/{id}")
-    public Piece getPieceById(@PathVariable Long id) {
-        // This is where you would query your database to retrieve the Piece with the given id
-        // For now, we're just returning a dummy piece
-        return new Piece(id, "Knight", "White");
-    }
+	@Autowired
+	private PieceRepository pieceRepo;
+
+	@GetMapping("/all")
+	public List<Piece> list() {
+		return pieceRepo.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Piece get(@PathVariable Long id) {
+		return pieceRepo.getReferenceById(id);
+	}
+
+	@PostMapping("/add")
+	public Piece create(@RequestBody final Piece piece) {
+		return pieceRepo.saveAndFlush(piece);
+	}
+
+	@PutMapping("/update")
+	public Piece update(@RequestBody final Piece piece) {
+		return pieceRepo.saveAndFlush(piece);
+	}
+
+@DeleteMapping("/delete/{id}")
+public void delete(@PathVariable Long id) {
+	pieceRepo.deleteById(id);
 }
