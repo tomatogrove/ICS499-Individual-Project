77446bdf87949ac8ec7ca534584278ebf15411cb
lormai9 <112415899+lormai9@users.noreply.github.com>
2023-02-13 18:27:19 -0600

    Add files via upload

---
+++ BoardGameProject/src/main/java/com/team4/repositories/QueenRepository.java
@@ -0,0 +1,10 @@
+
+package com.team4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team4.model.classes.Queen;
+
+public interface QueenRepository extends JpaRepository<Queen, Long> {
+
+}
