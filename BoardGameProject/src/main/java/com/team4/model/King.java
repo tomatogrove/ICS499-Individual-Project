823948f687bdf25e8b8183c72ffdc7bdcb8572df
guledmo8 <guledmohamed8@gmail.com>
2023-03-16 18:24:45 -0500

    added some logic to the King class

--- BoardGameProject/src/main/java/com/team4/model/King.java
+++ BoardGameProject/src/main/java/com/team4/model/King.java
@@ -9,7 +9,10 @@ import jakarta.persistence.Entity;
 @Entity
 public class King extends Piece {
 	
+	private boolean hasMoved;
+	
 	public King() {
+		hasMoved = false;
 		
 	}
 	
@@ -26,6 +29,59 @@ public class King extends Piece {
 		int x = getCurrentSpace().getX();
 		int y = getCurrentSpace().getY();
 		
+		
+		
+		// if the position 1 space ahead of the king is not occupied, add it as a possible move
+		if (!board.getSpace(x, y + 1).getOccupied()) {
+			possibleMoves.add(board.getSpace(x, y + 1));
+		}
+		
+		// if the position 1 space to the right of the king is not occupied, add it as a possible move
+		if (!board.getSpace(x + 1, y).getOccupied()) {
+			possibleMoves.add(board.getSpace(x + 1, y));
+		}
+		
+		// if the position 1 space behind the king is not occupied, add it as a possible move
+		if (!board.getSpace(x, y - 1).getOccupied()) {
+			possibleMoves.add(board.getSpace(x, y - 1));
+		}
+		
+		// if the position 1 space to the left of the king is not occupied, add it as a possible move
+		if (!board.getSpace(x - 1, y).getOccupied()) {
+			possibleMoves.add(board.getSpace(x - 1, y));
+		}
+		
+		
+		// if the position 1 space northwest of the king is not occupied, add it as a possible move
+		if (!board.getSpace(x - 1, y + 1).getOccupied()) {
+			possibleMoves.add(board.getSpace(x - 1, y + 1));
+		}
+		
+		// if the position 1 space northeast of the king is not occupied, add it as a possible move
+		if (!board.getSpace(x + 1, y + 1).getOccupied()) {
+			possibleMoves.add(board.getSpace(x + 1, y + 1));
+		}
+		
+		// if the position 1 space southwest of the king is not occupied, add it as a possible move
+		if (!board.getSpace(x - 1, y - 1).getOccupied()) {
+			possibleMoves.add(board.getSpace(x - 1, y - 1));
+		}
+		
+		// if the position 1 space southeast of the king is not occupied, add it as a possible move
+		if (!board.getSpace(x + 1, y - 1).getOccupied()) {
+			possibleMoves.add(board.getSpace(x + 1, y - 1));
+		}
+
+
+		
         return possibleMoves;
     }
+    
+    public boolean getHasMoved() {
+		return hasMoved;
+	}
+
+	public void setHasMoved(boolean hasMoved) {
+		this.hasMoved = hasMoved;
+	}
 }
