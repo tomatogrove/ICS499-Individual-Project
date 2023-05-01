6ec7d1d77212535f0021c2d4ca6c3d986b306ae9
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2023-04-30 20:23:44 -0500

    refactor pawn method

--- BoardGameProject/src/main/java/com/team6/model/pieces/Pawn.java
+++ BoardGameProject/src/main/java/com/team6/model/pieces/Pawn.java
@@ -25,50 +25,40 @@ public class Pawn extends Piece {
 
     @Override
     public List<Space> findPossibleMoves() {
+        List<Space> possibleMoves;
+
+        // movement of pawns is opposite (regarding vertical movement) for each color... white's logic first
+        if (getColor().equals(Color.WHITE)) {
+            possibleMoves = findPossibleMovesWithPolarity(1);
+        } else {
+            possibleMoves = findPossibleMovesWithPolarity(-1);
+        }
+
+        return possibleMoves;
+    }
+
+    private List<Space> findPossibleMovesWithPolarity(int yPolarity) {
         List<Space> possibleMoves = new ArrayList<>();
         Board board = getBoard();
         int x = getCurrentSpace().getX();
         int y = getCurrentSpace().getY();
 
-        // movement of pawns is opposite (regarding vertical movement) for each color... white's logic first
-        if (getColor().equals(Color.WHITE)) {
-            // if it is the first move, and the positions one and two spaces ahead are not occupied, add it as a possible move
-            if (!hasMoved && !board.findSpace(x, y + 1).isOccupied() && !board.findSpace(x, y + 2).isOccupied()) {
-                possibleMoves.add(board.findSpace(x, y + 2));
-            }
+        // if it is the first move, and the positions one and two spaces ahead are not occupied, add it as a possible move
+        if (!hasMoved && !board.findSpace(x, y + (yPolarity)).isOccupied() && !board.findSpace(x, y + (yPolarity * 2)).isOccupied()) {
+            possibleMoves.add(board.findSpace(x, y + (yPolarity * 2)));
+        }
 
-            for (int i = -1; i < 2; i++) {
-                Space space = board.findSpace(x + i, y + 1);
-                // ensure that the space exists on the board before adding it to the possible moves
-                if (space != null) {
-                    // if the space is a diagonal and it is occupied by an enemy color, then add it as a possible move (capture)
-                    if (space.getX() != x && space.isOccupied() && !space.getPiece().getColor().equals(getColor())) {
-                        possibleMoves.add(space);
+        for (int i = - 1; i < 2; i++) {
+            Space space = board.findSpace(x + i, y + yPolarity);
+            // ensure that the space exists on the board before adding it to the possible moves
+            if (space != null) {
+                // if the space is a diagonal, and it is occupied by an enemy color, then add it as a possible move (capture)
+                if (space.getX() != x && space.isOccupied() && !space.getPiece().getColor().equals(getColor())) {
+                    possibleMoves.add(space);
 
-                        //otherwise if the space is not occupied it is viable
-                    } else if (space.getX() == x && !space.isOccupied()) {
-                        possibleMoves.add(space);
-                    }
-                }
-            }
-        } else {
-            // if it is the first move, and the positions one and two spaces ahead are not occupied, add it as a possible move
-            if (!hasMoved && !board.findSpace(x, y - 1).isOccupied() && !board.findSpace(x, y - 2).isOccupied()) {
-                possibleMoves.add(board.findSpace(x, y - 2));
-            }
-
-            for (int i = -1; i < 2; i++) {
-                Space space = board.findSpace(x + i, y - 1);
-                // ensure that the space exists on the board before adding it to the possible moves
-                if (space != null) {
-                    // if the space is a diagonal and it is occupied by an enemy color, then add it as a possible move (capture)
-                    if (space.getX() != x && space.isOccupied() && !space.getPiece().getColor().equals(getColor())) {
-                        possibleMoves.add(space);
-
-                        //otherwise if the space is not occupied it is viable
-                    } else if (space.getX() == x && !space.isOccupied()) {
-                        possibleMoves.add(space);
-                    }
+                    //otherwise if the space is not occupied it is viable
+                } else if (space.getX() == x && !space.isOccupied()) {
+                    possibleMoves.add(space);
                 }
             }
         }
